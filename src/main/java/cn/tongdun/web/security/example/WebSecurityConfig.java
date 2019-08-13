9e2bea81a85e1b55abdccc9f4ffc6334d3e0ef1b
binsong.li <binsong.li@tongdun.cn>
2019-08-13 15:44:06 +0800

    update

--- src/main/java/cn/tongdun/web/security/example/WebSecurityConfig.java
+++ src/main/java/cn/tongdun/web/security/example/WebSecurityConfig.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
